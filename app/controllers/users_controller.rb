e1a32a4e046a9b3e4cfb8381effbcb33ece392b5
Joshua Ivie <joshua@lifeonline.com.ng>
2021-08-31 04:15:25 +0100

    Add Edit and Update to Controller
    Signed-off-by: Joshua Ivie <joshua@lifeonline.com.ng>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,15 +4,29 @@ class UsersController < ApplicationController
   end
   
   def create
-    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
-
     if @user.save
       redirect_to new_user_path
     else
       render :new
     end
   end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+      if @user.update_attributes(user_params)
+        flash[:success] = "User was successfully updated"
+        redirect_to @user
+      else
+        flash[:error] = "Something went wrong"
+        render 'edit'
+      end
+  end
+  
 
   private
   def user_params
